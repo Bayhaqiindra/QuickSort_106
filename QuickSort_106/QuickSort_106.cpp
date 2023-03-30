22570284e351e333a5977cf070a4d3a09c60895e
bayhaqiindra <bayhaqi30072004@gmail.com>
2023-03-30 21:15:44 +0700

    langkah algoritma ke 7

--- QuickSort_106/QuickSort_106.cpp
+++ QuickSort_106/QuickSort_106.cpp
@@ -65,3 +65,6 @@ void q_short(int low, int high)
 			cmp_count++;
 		}
 		cmp_count++;
+
+		//search for an element less than or equal to pivot
+		while ((arr[j] > pivot) && (j >= low))			//Langkah Algoritma No. 7
